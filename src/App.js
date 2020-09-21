a20577911c8adbc23c604c9fcf80605ddb044a50
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-09-21 08:36:14 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 
+
 var countDownDate = new Date("Oct 29, 2020 15:37:25").getTime();
 
 var x = setInterval(function() {
@@ -15,20 +16,25 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("coutdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("coutdown").innerHTML = "EXPIRED";
   }
 }, 1000);
 
 
+
+
 function App() {
   return (
-    <div className="App">
-      <p id="demo"></p>
+    <div className="background-img">
+      
+        <h1>Shadowlands Release</h1>
+        <p id="coutdown"></p>
+      
     </div>
   );
 }
